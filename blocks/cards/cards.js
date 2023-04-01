58b33c5c3ccdf0b3af033d19e23a406bad90f856
nehabd <44604522+Nehaadobe@users.noreply.github.com>
2023-04-01 21:13:30 +0530

    updated fragment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,14 +4,20 @@ export default async function decorate(block) {
 
   const locale = getMetadata('locale') || '/na/en';
   const placeholders = await fetchPlaceholders(locale);
-  console.log(placeholders)
-
+  
+  
   /* change to ul, li */
   const ul = document.createElement('ul');
+  
   [...block.children].forEach((row) => {
+    const moreText = document.createElement('a');
+    moreText.setAttribute('href', '#');
+    moreText.setAttribute("class","more");
+    moreText.innerHTML = placeholders.more;
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
+      li.append(moreText)
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
